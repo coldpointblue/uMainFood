af5b0bb0db6fdc4c68e08f25bc47551ff44bd9e5
coldpointblue <stayatplay@gmail.com>
2024-02-23 05:07:16 +0100

    Add SkeletonRestaurantRowView as placeholder for list.

---
+++ uMainFood/Views/SkeletonRestaurantRowView.swift
@@ -0,0 +1,35 @@
+//  ----------------------------------------------------
+//
+//  SkeletonRestaurantRowView.swift
+//  Version 1.0
+//
+//  Unique ID:  192328E8-D110-4027-A3AA-F23364CD43A8
+//
+//  part of the uMainFood™ product.
+//
+//  Written in Swift 5.0 on macOS 14.3
+//
+//  https://github.com/coldpointblue
+//  Created by Hugo Diaz on 2024-02-23.
+//  
+//  ----------------------------------------------------
+
+//  ----------------------------------------------------
+/*  Goal explanation:  Placeholder skeleton row while loading Restaurants   */
+//  ----------------------------------------------------
+
+
+import SwiftUI
+
+struct SkeletonRestaurantRowView: View {
+    var body: some View {
+        Text("…")
+    }
+}
+
+struct SkeletonRestaurantRow_Previews: PreviewProvider {
+    static var previews: some View {
+        SkeletonRestaurantRowView()
+    }
+}
+
