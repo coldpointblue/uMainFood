dccf4e420333d0ac6834cc764398bc9a481c98d3
coldpointblue <stayatplay@gmail.com>
2024-02-23 13:18:14 +0100

    Add HomeView screen that shows the list loading its data from the web.

--- uMainFood/Screens/HomeView.swift
+++ uMainFood/Screens/HomeView.swift
@@ -11,7 +11,7 @@
 //
 //  https://github.com/coldpointblue
 //  Created by Hugo Diaz on 2024-02-15.
-//  
+//
 //  ----------------------------------------------------
 
 //  ----------------------------------------------------
@@ -22,17 +22,42 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject var viewModel = RestaurantListViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            GeometryReader { geometry in
+                
+                if viewModel.isLoading || viewModel.allRestaurants.isEmpty {
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            ProgressView()
+                                .scaleEffect(3)
+                                .padding()
+                            Spacer()
+                        }
+                        Spacer()
+                    }
+                } else {
+                    RestaurantsListView(viewModel: viewModel)
+                }
+            }
+            .onReceive(viewModel.$allRestaurants) { allRestaurants in
+                viewModel.updateUIAfterFetching()
+            }
+            .navigationTitle("Restaurants")
+            .toolbar(.hidden, for: .tabBar)
+            .task {
+                viewModel.fetchRestaurantsAndFilters()
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    HomeView()
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
 }
