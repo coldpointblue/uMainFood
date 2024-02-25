5f3fe6852ef9bd909a62df162e92a1828d8cea73
coldpointblue <stayatplay@gmail.com>
2024-02-25 12:46:57 +0100

    Adapt FilterToggleButton to bind with incoming data + smoother animation between them.

--- uMainFood/Views/Components/FilterToggleButton.swift
+++ uMainFood/Views/Components/FilterToggleButton.swift
@@ -8,6 +8,7 @@ struct FilterToggleButton<Icon: View>: View {
     @Binding var isOn: Bool
     let icon: Icon
     let tag: String
+    
     private let onColor: Color = .selected
     private let offColor: Color = .grayLightShadeColor
     private let iconWidth : CGFloat = 48
@@ -26,7 +27,9 @@ struct FilterToggleButton<Icon: View>: View {
             maximumDistance: 10,
             perform: {},
             onPressingChanged: { isPressing in
-                isPressed = isPressing
+                withAnimation {
+                    isPressed = isPressing
+                }
             }
         )
     }
@@ -35,8 +38,8 @@ struct FilterToggleButton<Icon: View>: View {
         HStack {
             icon
                 .frame(width: iconWidth, height: iconWidth)
-                .background(.clear)
-                .cornerRadius(iconWidth/2)
+                .background(isOn ? onColor : offColor)
+                .cornerRadius(iconWidth / 2)
             
             Text(tag)
                 .font(.title3)
@@ -55,29 +58,28 @@ struct FilterToggleButton<Icon: View>: View {
     }
 }
 
-// Example Usage with BlankIcon
-struct BlankIcon: View {
-    var body: some View {
-        ZStack {
-            Image(systemName: "circle.dashed")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Image(systemName: "icloud.slash")
-                .imageScale(.large)
-        }
-    }
-}
-
 struct FilterToggleButton_Previews: PreviewProvider {
     static var previews: some View {
         FilterToggleButtonStatePreview()
+    }
+    
+    struct BlankIconExample: View {
+        var body: some View {
+            ZStack {
+                Image(systemName: "circle.dashed")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image(systemName: "icloud.slash")
+                    .imageScale(.large)
+            }
+        }
     }
     
     private struct FilterToggleButtonStatePreview: View {
         @State private var isOn = false
         
         var body: some View {
-            FilterToggleButton(isOn: $isOn, icon: BlankIcon(), tag: "Fast Delivery")
+            FilterToggleButton(isOn: $isOn, icon: BlankIconExample(), tag: "Fast Delivery")
                 .previewLayout(.sizeThatFits)
                 .padding()
                 .previewDisplayName("Filter Toggle Button")
