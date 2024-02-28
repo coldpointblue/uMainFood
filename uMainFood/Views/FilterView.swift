f89fbaf7ff2a0b0a7ad604916b0f5bc1df7f27a5
coldpointblue <stayatplay@gmail.com>
2024-02-28 16:39:05 +0100

    Refactor FilterView for better readability.

--- uMainFood/Views/FilterView.swift
+++ uMainFood/Views/FilterView.swift
@@ -9,24 +9,28 @@ struct FilterView: View {
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 0) {
-                ForEach(viewModel.completeFilters, id: \.filter.id) { (completeFilter: API.Model.Filter.Complete) in
-                    let isSelected = viewModel.selectedFilterIds.contains(completeFilter.filter.id)
-                    let iconImage = completeFilter.image.map { Image(uiImage: $0) } ?? Image.missingWebData()
-                    
-                    FilterToggleButton(isOn: Binding(
-                        get: { isSelected },
-                        set: { newValue in
-                            if newValue {
-                                viewModel.selectedFilterIds.insert(completeFilter.filter.id)
-                            } else {
-                                viewModel.selectedFilterIds.remove(completeFilter.filter.id)
-                                
-                            }
-                        }
-                    ), icon: iconImage, tag: completeFilter.filter.name)
+                ForEach(viewModel.completeFilters, id: \.filter.id) { completeFilter in
+                    filterToggleButton(for: completeFilter)
                 }
             }
         }
+    }
+    
+    @ViewBuilder
+    private func filterToggleButton(for completeFilter: API.Model.Filter.Complete) -> some View {
+        let isSelected = viewModel.selectedFilterIds.contains(completeFilter.filter.id)
+        let iconImage = completeFilter.image.map { Image(uiImage: $0) } ?? Image.missingWebData()
+        
+        FilterToggleButton(isOn: Binding(
+            get: { isSelected },
+            set: { newValue in
+                if newValue {
+                    viewModel.selectedFilterIds.insert(completeFilter.filter.id)
+                } else {
+                    viewModel.selectedFilterIds.remove(completeFilter.filter.id)
+                }
+            }
+        ), icon: iconImage, tag: completeFilter.filter.name)
     }
 }
 
